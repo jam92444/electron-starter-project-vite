646b822a41ae7b8cdcbcfbc08f769bf22349a046
jam92444 <jamalyusuf579@gmail.com>
2025-12-03 22:30:45 +0530

    Added: starter project

---
+++ src/renderer.jsx
@@ -0,0 +1,16 @@
+
+import { createRoot } from "react-dom/client";
+
+
+const App =()=>{
+  return (
+    <h1>Hello from react</h1>
+  )
+}
+
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+
+
+root.render(<App/>)
